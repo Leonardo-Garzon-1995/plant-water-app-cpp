a492312d8562b75337031c86edf0c2a35f90e925
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2025-12-03 19:27:53 +0100

    create: helpers_to_main file
    using modules in C++

--- main.cpp
+++ main.cpp
@@ -1,36 +1,11 @@
 #include <iostream>
 
-std::string needs_water(int days, bool is_succulent) {
-    if (!is_succulent && days > 3) {
-        return "Time to water your plant.";
-    } else if (is_succulent && days <= 15) {
-        return "Don't water yet.";
-    } else if (is_succulent && days > 15) {
-        return "Time to water your succulent.";
-    } else {
-        return "no need to water";
-    }
-}
+// Using modular reference
+// This functions are defined in helpers_to_main.cpp
+std::string needs_water(int days, bool is_succulent);
+int get_days_input();
+bool get_is_succulent_input();
 
-int get_days_input() {
-    int days;
-    std::cout << "Enter the number of days: ";
-    std::cin >> days;
-    return days;
-}
-
-bool get_is_succulent_input() {
-    bool is_succulent;
-    std::cout << "Is the plant succulent? (y/n): ";
-    std::string input;
-    std::cin >> input;
-    if (input == "yes" || input == "y" || input == "Y" || input == "Yes") {
-        is_succulent = true;
-    } else {
-        is_succulent = false;
-    }
-    return is_succulent;
-}
 int main() {
 
     std::cout << "Welcome to the Plant Watering App!" << std::endl;
